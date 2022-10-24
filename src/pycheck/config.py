8449f052dd906302d9a87e7905f399d074d6a8d6
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-10-24 23:20:14 +0100

    Add config for ut1-pop1

--- src/pycheck/config.py
+++ src/pycheck/config.py
@@ -1,6 +1,42 @@
 CONFIG = {
     'ut1-pop1-ej1': {
-        'arg_casts': (int, int),
-        'cases': (((3, 4), 12), ((5, 3), 15), ((-3, 3), -9)),
-    }
+        'arg_casts': [int, int],
+        'cases': [
+            [[5, 7], 68.08392021690038],
+            [[6, 2], 36.53589838486224],
+            [[21, 4], 447.83484861008833],
+        ],
+    },
+    'ut1-pop1-ej2': {
+        'arg_casts': [int],
+        'cases': [
+            [[99], 4],
+            [[201], 4],
+            [[3219], 6],
+        ],
+    },
+    'ut1-pop1-ej3': {
+        'arg_casts': [str, int, str],
+        'cases': [
+            [['e', 2, 'diferencia'], 'diforoncia'],
+            [['u', -2, 'uruguay'], 'irigiay'],
+            [['a', 4, 'anatolia'], 'unutoliu'],
+        ],
+    },
+    'ut1-pop1-ej4': {
+        'arg_casts': [str],
+        'cases': [
+            [['A131F7'], '(161,49,247)'],
+            [['FF11FF'], '(255,17,255)'],
+            [['123456'], '(18,52,86)'],
+        ],
+    },
+    'ut1-pop1-ej5': {
+        'arg_casts': [str],
+        'cases': [
+            [['hola probando'], 'hola-probando'],
+            [['áéíóú'], 'aeiou'],
+            [['TWIST & SHOUT'], 'twist-&-shout'],
+        ],
+    },
 }
