cf7477e2c66ada4072c1598b86e1e1572f4d4771
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-10-24 23:37:29 +0100

    Add config for ut2-pop0

--- src/pycheck/config.py
+++ src/pycheck/config.py
@@ -1,4 +1,20 @@
 CONFIG = {
+    'ut1-pop0-ej1': {
+        'arg_casts': [int],
+        'cases': [
+            [[31256], (8, 40, 56)],
+            [[3601], (1, 0, 1)],
+            [[9099], (2, 31, 39)],
+        ],
+    },
+    'ut1-pop0-ej2': {
+        'arg_casts': [str],
+        'cases': [
+            [['GGTTACCAACCCAGTCGAAAATTTGGTCAGGG'], (28.125, 28.125, 21.875, 21.875)],
+            [['ATGGGATCCTAGCCCCTTAG'], (20.0, 25.0, 25.0, 30.0)],
+            [['GGATTCTGAGAATCCGCTAATGCC'], (25.0, 25.0, 25.0, 25.0)],
+        ],
+    },
     'ut1-pop1-ej1': {
         'arg_casts': [int, int],
         'cases': [
