58e2b190977de4c8cd2548c768e3d26969924116
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-10-25 16:59:59 +0100

    Add case listing

--- src/pycheck/core.py
+++ src/pycheck/core.py
@@ -7,7 +7,7 @@ class PyChecker:
     def __init__(self, exercise_id: str, cmd: Iterable, target_func: callable):
         self.exercise_id = exercise_id
         self.filename, *self.cli_args = cmd
-        self.flag = self.cli_args[0].strip()
+        self.flag = self.cli_args[0] if len(self.cli_args) > 0 else ''
         self.target_func = target_func
         self.config = utils.get_config(exercise_id)
 
@@ -26,9 +26,26 @@ class PyChecker:
         if (result := self.target_func(*args)) is not None:
             print(result)
 
+    def list_cases(self):
+        for args, expected_output in self.config['cases']:
+            print(f'{args}: {expected_output}')
+
+    def usage(self):
+        print(
+            '''Modo de uso:
+python <program.py> [ARGS]
+    -k: Comprueba tu programa contra los casos especificados.
+    -l: Muestra un listado de los casos (entradas y salidas esperadas).
+    [ARGS]: Ejecuta tu programa con los argumentos indicados.'''
+        )
+
     def run(self):
         match self.flag:
             case '-k':
                 self.run_cases()
+            case '-l':
+                self.list_cases()
+            case '':
+                self.usage()
             case _:
                 self.run_custom()
