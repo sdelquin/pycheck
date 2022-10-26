50b9bf9cb9f2cb2621f2de9b337732bc0b365df5
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-10-26 11:43:34 +0100

    Add new test cases

---
+++ src/pycheck/config/pro/ut2/pop0.py
@@ -0,0 +1,17 @@
+ej1 = {
+    'arg_casts': [str],
+    'cases': [
+        [['sergio delgado quintero'], 'srg dlgd qntr'],
+        [['PEPE BENAVENTE'], 'PP BNVNT'],
+        [['volando VOY, volando VENGO'], 'vlnd VY, vlnd VNG'],
+    ],
+}
+
+ej2 = {
+    'arg_casts': [str],
+    'cases': [
+        [['probando'], 106.625],
+        [['Buena Suerte'], 96.25],
+        [['Yo escribo Python'], 97.0],
+    ],
+}
