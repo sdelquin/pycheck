1054a82711fd227acf4a99908399bd0c4541b58e
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-09 16:52:32 +0000

    Just testing project script 1

--- src/pycheck/main.py
+++ src/pycheck/main.py
@@ -6,3 +6,7 @@ from pycheck.lib.core import PyChecker
 def check(target_func: callable, check_cases: Iterable, cmd: Iterable):
     pychecker = PyChecker(target_func, check_cases, cmd)
     pychecker.run()
+
+
+def hello():
+    print('Hellouuu')
