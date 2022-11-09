8e4199b308e40739e20acac46f600b71d5a94c84
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-09 23:36:18 +0000

    Change cases variable name

--- src/pycheck/main.py
+++ src/pycheck/main.py
@@ -6,7 +6,7 @@ from pycheck.lib.utils import load_module
 
 def check(*args):
     module = load_module(args[0])
-    pychecker = PyChecker(module.run, module.CHECK_CASES, args)
+    pychecker = PyChecker(module.run, module.__PYCHECK_CASES__, args)
     pychecker.run()
 
 
