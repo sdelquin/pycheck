ed6ad22b973aa46bdce8b684817c37581730f02f
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-20 21:01:52 +0000

    Fix exercise

--- src/pycheck/exercises/eec030ce24d310fd6cc6be4a58e4d54b.py
+++ src/pycheck/exercises/eec030ce24d310fd6cc6be4a58e4d54b.py
@@ -1,7 +1,6 @@
 DESCRIPTION = '''
-Calcule el área de un círculo a partir de su radio. Tenga en cuenta:
-- Usar PI con 2 cifras decimales.
-- Devolver el resultado con 2 cifras decimales.
+Calcule el área de un círculo a partir de su radio. Tenga en cuenta usar PI con 2 cifras
+decimales.
 '''
 
 ENTRYPOINT = {
@@ -16,5 +15,5 @@ ENTRYPOINT = {
 CHECK_CASES = [
     [[4], [50.24]],
     [[100], [31400.00]],
-    [[39.8], [4973.89]],
+    [[39.8], [4973.8856]],
 ]
