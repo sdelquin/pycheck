c037efde71173648d810e5200c74b18ca18cab31
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-22 09:47:53 +0000

    Add exercises

---
+++ src/pycheck/exercises/355e6d78b9c4ce51ab94768dfbe00964.py
@@ -0,0 +1,29 @@
+DESCRIPTION = '''
+Un reloj muestra horas, minutos y segundos a partir de medianoche.
+Calcule el número de milisegundos desde media noche a partir de estos tres parámetros.
+
+0 <= horas <= 23
+0 <= minutos <= 59
+0 <= segundos <= 59
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['hours', int],
+        ['minutes', int],
+        ['seconds', int],
+    ],
+    'RETURN': [
+        ['time_since_midnight', float],
+    ],
+}
+
+CHECK_CASES = [
+    [[0, 1, 1], [61000]],
+    [[2, 1, 0], [7260000]],
+    [[10, 45, 30], [38730000]],
+]
+
+PYCOINS = 2
+
+SOURCE = 'https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a'
---
+++ src/pycheck/exercises/a45cd7762c9434f2380dff19ddbed8a5.py
@@ -0,0 +1,31 @@
+DESCRIPTION = '''
+El "o exclusivo" (xor) es una operación lógica que sigue las siguientes reglas:
+- False xor False -> False
+- True xor False -> True
+- False xor True -> True
+- True xor True -> False
+
+Básicamente sólo es verdadero cuando una (y solo una) de las dos entradas es verdadera.
+Escriba un programa que calcule el xor a partir de dos parámetros de entrada.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['v1', bool],
+        ['v2', bool],
+    ],
+    'RETURN': [
+        ['xor', bool],
+    ],
+}
+
+CHECK_CASES = [
+    [[False, False], [False]],
+    [[True, False], [True]],
+    [[False, True], [True]],
+    [[True, True], [False]],
+]
+
+PYCOINS = 2
+
+SOURCE = 'https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c'
