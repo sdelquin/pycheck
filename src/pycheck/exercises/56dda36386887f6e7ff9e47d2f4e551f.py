d6a55e7c6971227943c7274f32ce66b72502c581
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-24 15:42:35 +0000

    Add exercise

---
+++ src/pycheck/exercises/56dda36386887f6e7ff9e47d2f4e551f.py
@@ -0,0 +1,22 @@
+DESCRIPTION = '''
+Dada una lista de valores numéricos enteros, obtenga su mínimo valor.
+
+Prohibido utilizar:
+- La función "built-in" min().
+- La función "built-in" sorted().
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['values', list],
+    ],
+    'RETURN': [
+        ['min_value', int],
+    ],
+}
+
+CHECK_CASES = [
+    [[[-11, 10, -6, 15, -1]], [-11]],
+    [[[5, 9, -6, 9, -2, -9, -7, 2]], [-9]],
+    [[[-7, -5, -5, -8, -3]], [-8]],
+]
