e2a2053a6f0c90fab71f7cd2c3d70671e1a89637
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-24 12:29:53 +0000

    Add exercise

---
+++ src/pycheck/exercises/54695cec8aaf96c69c093a682b97224d.py
@@ -0,0 +1,22 @@
+DESCRIPTION = '''
+Dada una hora en formato HH:MM y un número de minutos (en valor entero), calcule la hora
+resultante (en formato HH:MM) si sumamos los minutos a la hora de entrada.
+
+Trabaje con formato de 24 horas.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['time', str],
+        ['offset', int],
+    ],
+    'RETURN': [
+        ['final_time', str],
+    ],
+}
+
+CHECK_CASES = [
+    [['17:15', 240], ['21:15']],
+    [['8:05', 265], ['12:30']],
+    [['22:55', 315], ['4:10']],
+]
