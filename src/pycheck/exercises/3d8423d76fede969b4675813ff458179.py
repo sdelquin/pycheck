bf0d59a340d3ec74c9c4c0d57907e8b51814ca78
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-24 15:54:10 +0000

    Add exercise

---
+++ src/pycheck/exercises/3d8423d76fede969b4675813ff458179.py
@@ -0,0 +1,28 @@
+DESCRIPTION = '''
+Dada una lista de valores numéricos, calcule la suma de todos sus elementos sin tener en
+cuenta el valor máximo y el valor mínimo.
+
+NOTAS:
+- Pueden haber varios valores máximos o varios valores mínimos (repeticiones).
+- Puede venir la lista vacía como entrada.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['values', list],
+    ],
+    'RETURN': [
+        ['tsum', int],
+    ],
+}
+
+CHECK_CASES = [
+    [[[6, 2, 1, 8, 10]], [16]],
+    [[[1, 1, 11, 2, 3]], [5]],
+    [[[1]], [0]],
+    [[[1, 1]], [0]],
+    [[[1, 2]], [0]],
+    [[[]], [0]],
+]
+
+SOURCE = 'https://www.codewars.com/kata/576b93db1129fcf2200001e6'
