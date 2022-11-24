2f14a715da492cd44e809395d9b7b07a15fff028
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-24 12:38:41 +0000

    Add exercise

---
+++ src/pycheck/exercises/3a4476b649f265385254a663b1366e2f.py
@@ -0,0 +1,21 @@
+DESCRIPTION = '''
+Dada una lista de números, calcule la suma de todos los valores positivos.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['numbers', list],
+    ],
+    'RETURN': [
+        ['sum_positive', int],
+    ],
+}
+
+CHECK_CASES = [
+    [[[1, -4, 7, 12]], [20]],
+    [[[-1, -1, -1, -1, -1, -1]], [0]],
+    [[[1, -1, 2, -2, 3, -3, 4, -4, 5, -5]], [15]],
+    [[[]], [0]],
+]
+
+SOURCE = 'https://www.codewars.com/kata/5715eaedb436cf5606000381'
