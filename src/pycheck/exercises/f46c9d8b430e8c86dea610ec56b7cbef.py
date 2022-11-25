4b9ad95ba4583cf8639f776b268b1f06e563fe62
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-25 12:23:42 +0000

    Add exercise

---
+++ src/pycheck/exercises/f46c9d8b430e8c86dea610ec56b7cbef.py
@@ -0,0 +1,28 @@
+DESCRIPTION = '''
+El objetivo de este ejercicio es simular el comportamiento de un intervalo matemático de
+dos números enteros.
+
+Dada una cadena de texto que contiene una descripción del intervalo, genere una lista con
+todos los números incluidos en dicho intervalo.
+
+[a,b]: Todos los números entre a y b, con a y b incluidos en el intervalo.
+(a,b]: Todos los números entre a y b, sin incluir a pero incluyendo a b.
+[a,b): Todos los números entre a y b, inlucyendo a pero sin incluir a b.
+(a,b): Todos los números entre a y b, sin incluir a ni b.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['interval', str],
+    ],
+    'RETURN': [
+        ['irange', list],
+    ],
+}
+
+CHECK_CASES = [
+    [['[3,10]'], [[3, 4, 5, 6, 7, 8, 9, 10]]],
+    [['(3,10]'], [[4, 5, 6, 7, 8, 9, 10]]],
+    [['[3,10)'], [[3, 4, 5, 6, 7, 8, 9]]],
+    [['[3,10]'], [[3, 4, 5, 6, 7, 8, 9, 10]]],
+]
