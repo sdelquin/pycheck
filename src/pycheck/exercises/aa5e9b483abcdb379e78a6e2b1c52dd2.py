453a7fab71ac021803b753a80ecdc52a3cff2723
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-25 21:48:56 +0000

    Add exercise

--- src/pycheck/exercises/aa5e9b483abcdb379e78a6e2b1c52dd2.py
+++ src/pycheck/exercises/aa5e9b483abcdb379e78a6e2b1c52dd2.py
@@ -3,7 +3,7 @@ TITLE = 'Calculadora lógica'
 DESCRIPTION = '''
 Dada una lista de valores booleanos y un operador lógico, calcule el resultado booleano tras aplicar el operador a la lista de valores de entrada de forma secuencial.
 
-Tenga en cuenta que los dos posibles operadores de entrada son 'and' y 'or' y que siempre van a aparecer en minúsculas.
+Tenga en cuenta que los dos únicos posibles operadores de entrada son 'and' y 'or' y que siempre van a aparecer en minúsculas.
 '''
 
 ENTRYPOINT = {
@@ -18,4 +18,7 @@ ENTRYPOINT = {
 
 CHECK_CASES = [
     [[[True, True, False], 'and'], [False]],
+    [[[True, True, False], 'or'], [True]],
+    [[[True, True, True], 'and'], [True]],
+    [[[False, False, False], 'and'], [False]],
 ]
