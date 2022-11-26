e06921f741c40da1e64cb8bcaaa1e07632eab8aa
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-26 17:25:49 +0000

    Add exercise

---
+++ src/pycheck/exercises/f7db169d317648e56841a7f880af440e.py
@@ -0,0 +1,28 @@
+ID = 'npartition'
+
+TITLE = 'Partición por el número'
+
+TOPIC = 'lists'
+
+DESCRIPTION = '''
+Dada una lista de entrada de valores numéricos y dado un número de referencia, obtenga una lista de dos listas, donde la primera lista contenga todos los números de la lista de entrada menores que el número de referencia y donde la segunda lista contenga todos los números de la lista de salida mayores o iguales que el número de referencia.
+
+Mantenga el mismo orden en el que vienen los números de la lista de entrada.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['values', list],
+        ['ref_value', int],
+    ],
+    'RETURN': [
+        ['npartition', list],
+    ],
+}
+
+CHECK_CASES = [
+    [[[4, 3, 2, 9, 8, 5], 4], [[[3, 2], [4, 9, 8, 5]]]],
+    [[[5, 3, 8, 7, 11, 19, 12, 15], 10], [[[5, 3, 8, 7], [11, 19, 12, 15]]]],
+    [[[1, 2, 3], 10], [[[1, 2, 3], []]]],
+    [[[1, 2, 3], -10], [[[], [1, 2, 3]]]],
+]
