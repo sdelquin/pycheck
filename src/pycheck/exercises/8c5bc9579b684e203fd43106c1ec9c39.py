1fc6d9742fac80bb4a4b92c47ccb16e47e5671f3
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-11-30 16:15:31 +0000

    Add exercise

---
+++ src/pycheck/exercises/8c5bc9579b684e203fd43106c1ec9c39.py
@@ -0,0 +1,22 @@
+TITLE = 'Contando letras'
+
+DESCRIPTION = '''
+Usando un diccionario, cuente el número de veces que se repite cada letra en una cadena de texto dada.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['sentence', str],
+    ],
+    'RETURN': [
+        ['counter', dict],
+    ],
+}
+
+CHECK_CASES = [
+    [['boom'], [{'b': 1, 'o': 2, 'm': 1}]],
+    [['ada'], [{'a': 2, 'd': 1}]],
+    [['tree'], [{'t': 1, 'r': 1, 'e': 2}]],
+    [['debugging'], [{'d': 1, 'e': 1, 'b': 1, 'u': 1, 'g': 3, 'i': 1, 'n': 1}]],
+    [[''], [{}]],
+]
