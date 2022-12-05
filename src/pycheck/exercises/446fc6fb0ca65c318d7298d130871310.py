d3dd297bdfac871fd41910c36985461541a0240e
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-05 10:11:39 +0000

    Add exercise

---
+++ src/pycheck/exercises/446fc6fb0ca65c318d7298d130871310.py
@@ -0,0 +1,23 @@
+TITLE = '¿Hay stock?'
+
+DESCRIPTION = '''
+Usted es encargado de una empresa de merchandising y dispone de un cierto stock para los artículos. El cliente hace un pedido especificando el artículo y la cantidad requerida.
+
+Usted debe comprobar si existe stock para la cantidad solicitada del artículo indicando _True_ o _False_.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['stock', dict],
+        ['merch', str],
+        ['amount', int],
+    ],
+    'RETURN': [
+        ['available', bool],
+    ],
+}
+
+CHECK_CASES = [
+    [[{'pen': 20, 'cup': 11, 'keyring': 40}, 'cup', 9], [True]],
+    [[{'pen': 20, 'cup': 11, 'keyring': 40}, 'pen', 21], [False]],
+]
