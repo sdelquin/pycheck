3155ca8d498c9a7dc55f484b6448d7fe731f0086
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-01-11 10:02:01 +0000

    Add exercise about loops

---
+++ src/pycheck/exercises/fe29ec414e4057a256a54f65da7d3fc8.py
@@ -0,0 +1,31 @@
+TITLE = 'Un set al tenis'
+
+DESCRIPTION = '''
+Escenario: Partido de Tenis. Dada una cadena de texto con letras A o B indicando si el jugador A ha ganado un punto o si el jugador B ha ganado un punto, haga un programa en Python que determine el resultado del set.
+
+Hay que tener en cuenta una posible situación de _tie-break_ cuando ambos jugadores empatan a 6 juegos. En este caso ganará el juego quien llegue al menos a 7 puntos con distancia de 2.
+'''
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['points', str],
+    ],
+    'RETURN': [
+        ['games_player1', int],
+        ['games_player2', int],
+    ],
+}
+
+CHECK_CASES = [
+    [['AABBAABABBBABABABBBAAABBBABAABBABBAABBBABABBAAAABBBBABBBAB'], [3, 6]],
+    [
+        [
+            'AABAABABBABAAABABBBAABABABABABABABBBBABBABAAAAABBAABAABBABBAABBAAABBBAABAAABBBBAAAABBBBAAA'
+        ],
+        [7, 6],
+    ],
+    [
+        ['ABAABABBBAAABBAABBAABABBBABABBABABAAABBBBAAAAAABBBBABBAABBBABBAAABBAAAAAABBABAA'],
+        [7, 5],
+    ],
+]
