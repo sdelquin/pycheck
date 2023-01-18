52812e1f07523a2c79582fa2adcb2489bba98d43
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-01-18 12:08:24 +0000

    Fix exercise

--- src/pycheck/exercises/f8594cddbfa61af6c6be5590732831f5.py
+++ src/pycheck/exercises/f8594cddbfa61af6c6be5590732831f5.py
@@ -3,7 +3,7 @@ TITLE = 'Población promedio'
 DESCRIPTION = '''
 Partiendo de un diccionario de ciudades (_claves_) y poblaciones (_valores_) -suponiendo que estas ciudades son las únicas que existen en el planeta- calcule el porcentaje de población relativo de cada una de ellas con respecto al total, dando como salida un diccionario.
 
-Obtena la media de población con una precisión de **3 decimales**.
+Obtenga la media de población con una precisión de **3 decimales**.
 '''
 
 ENTRYPOINT = {
