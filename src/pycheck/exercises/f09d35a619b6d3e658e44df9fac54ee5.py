c95fd4572c5ee3cdcd14904ec0cf01d48a1f74c4
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-01-25 19:49:23 +0000

    Fix exercise

--- src/pycheck/exercises/f09d35a619b6d3e658e44df9fac54ee5.py
+++ src/pycheck/exercises/f09d35a619b6d3e658e44df9fac54ee5.py
@@ -4,6 +4,10 @@ DESCRIPTION = '''
 Dadas dos cadenas de texto, obtenga una nueva cadena de texto con las letras consonantes que se repiten en ambas frases. Ignore los espacios en blanco y muestre la cadena de salida con sus letras ordenadas.
 
 Este ejercicio también se puede resolver mediante _conjuntos por comprensión_.
+
+Notas:
+- Las vocales pueden estar en minúsculas o en mayúsculas.
+- Una consonante minúscula es equivalente a una consonantes mayúscula.
 '''
 
 ENTRYPOINT = {
@@ -17,7 +21,7 @@ ENTRYPOINT = {
 }
 
 CHECK_CASES = [
-    [['Flat is better than nested', 'Readability counts'], ['bdlnst']],
-    [['Now is better than never', 'Beautiful is better than ugly'], ['bhnrst']],
+    [['Flat is bEtter than nested', 'Readability counts'], ['bdlnrst']],
+    [['Now is better thAn never', 'Beautiful is better than ugly'], ['bhnrst']],
     [['Hello', 'Bye'], ['']],
 ]
