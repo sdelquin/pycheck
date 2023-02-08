78a814b713ed251eb3ffc131091a051f37adb317
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-08 12:32:30 +0000

    Add exercise

---
+++ src/pycheck/exercises/cb89593d1f9fe3ecdb2178215eee80ae.py
@@ -0,0 +1,24 @@
+TITLE = 'Calculando el factorial de un número'
+
+EMPTY_TEMPLATE = True
+
+DESCRIPTION = '''
+Escriba una función que calcule el [factorial](https://es.wikipedia.org/wiki/Factorial) de un número _n_. El factorial de un número _n_ se define como:
+```
+n! = n · (n - 1) · (n - 2) · ... · 1
+```
+'''
+
+ENTRYPOINT = {
+    'NAME': 'factorial',
+    'PARAMS': [
+        ['n', int],
+    ],
+    'RETURN': [
+        ['fact', int],
+    ],
+}
+
+CHECK_CASES = [
+    [[5], [120]],
+]
