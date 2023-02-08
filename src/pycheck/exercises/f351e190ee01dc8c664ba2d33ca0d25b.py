e631161f5b172e1f3ea3c7d1143083d126b751ba
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-08 11:29:06 +0000

    Add exercise

---
+++ src/pycheck/exercises/f351e190ee01dc8c664ba2d33ca0d25b.py
@@ -0,0 +1,28 @@
+TITLE = 'Suma de cuadrados'
+
+CREATE_TEMPLATE = False
+
+DESCRIPTION = '''
+Escriba una función en Python que reproduzca lo siguiente:
+```
+           2    2
+f(x, y) = x  + y 
+```
+'''
+
+ENTRYPOINT = {
+    'NAME': 'f',
+    'PARAMS': [
+        ['x', int],
+        ['y', int],
+    ],
+    'RETURN': [
+        ['result', int],
+    ],
+}
+
+CHECK_CASES = [
+    [[3, 4], [25]],
+    [[-5, -7], [74]],
+    [[-2, 8], [68]],
+]
