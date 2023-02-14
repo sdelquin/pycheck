f707c8f29022a7b92c2b9f392bd419c8d5b47224
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-14 17:04:57 +0000

    Add exercise

---
+++ src/pycheck/exercises/53fec5831494fae778e48a63079c684b.py
@@ -0,0 +1,54 @@
+TITLE = 'Generando cuadrados'
+
+EMPTY_TEMPLATE = True
+
+DESCRIPTION = '''
+Escriba una función que reciba un parámetro _n_ y que incluya **una expresión generadora** para calcular los _n_ primeros números enteros elevados al cuadrado.
+
+Notas:
+- La función debe **devoler una lista** de los valores al cuadrado.
+- _n_ ∈ {0, ℤ+}
+'''
+
+ENTRYPOINT = {
+    'NAME': 'gen_sq',
+    'PARAMS': [
+        ['n', int],
+    ],
+    'RETURN': [
+        ['squared', list],
+    ],
+}
+
+CHECK_CASES = [
+    [[0], [[]]],
+    [[10], [[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]]],
+    [[15], [[0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196]]],
+    [
+        [20],
+        [
+            [
+                0,
+                1,
+                4,
+                9,
+                16,
+                25,
+                36,
+                49,
+                64,
+                81,
+                100,
+                121,
+                144,
+                169,
+                196,
+                225,
+                256,
+                289,
+                324,
+                361,
+            ]
+        ],
+    ],
+]
