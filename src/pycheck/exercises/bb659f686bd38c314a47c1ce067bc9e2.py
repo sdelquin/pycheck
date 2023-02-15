61d8ac04695f81ac31cb521bec9fc8881427dae1
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-02-15 13:02:19 +0000

    Add exercises

---
+++ src/pycheck/exercises/bb659f686bd38c314a47c1ce067bc9e2.py
@@ -0,0 +1,30 @@
+TITLE = 'Calculando el factorial de un número (con recursividad)'
+
+EMPTY_TEMPLATE = True
+
+DESCRIPTION = '''
+Escriba una **función recursiva** que calcule el [factorial](https://es.wikipedia.org/wiki/Factorial) de un número _n_. El factorial de un número _n_ se define como:
+```
+n! = n · (n - 1) · (n - 2) · ... · 1
+```
+
+Notas:
+- 0! = 1
+- El factorial de un número negativo no se puede calcular.
+'''
+
+ENTRYPOINT = {
+    'NAME': 'factorial',
+    'PARAMS': [
+        ['n', int],
+    ],
+    'RETURN': [
+        ['fact', int],
+    ],
+}
+
+CHECK_CASES = [
+    [[5], [120]],
+    [[0], [1]],
+    [[-1], [None]],
+]
