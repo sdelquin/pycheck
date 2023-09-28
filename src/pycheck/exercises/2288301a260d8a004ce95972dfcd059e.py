6fdffa5244e19aa240784117b2a477bae5d99a45
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-09-28 10:58:21 +0100

    Fix exercise

--- src/pycheck/exercises/2288301a260d8a004ce95972dfcd059e.py
+++ src/pycheck/exercises/2288301a260d8a004ce95972dfcd059e.py
@@ -1,12 +1,19 @@
 TITLE = 'Interés compuesto'
 
 DESCRIPTION = '''
-Compute el valor futuro de una cantidad de dinero (capital final), a partir del capital inicial, el tipo de interés y el número de años. Fórmula del interés compuesto:
+Compute el capital final (_Cf_) a partir del capital inicial (_Ci_), el tipo de interés (_i_) y el número de años (_n_). Fórmula del interés compuesto:
 ```
-                 n
-C  = C  ⋅ (1 + i)
+                     n
+C  = C  ⋅ (1 + i/100)
  f    i
 ```
+
+La correspondencia con las variables del programa es la siguiente:
+
+- _Ci_ → amount
+- _i_ → rate
+- _n_ → years
+- _Cf_ → future_amount
 '''
 
 ENTRYPOINT = {
