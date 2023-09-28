99dc9b2380d2b7ca0bb505f25cbe98e5a1bb8414
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-09-28 16:34:42 +0100

    Fix exercise

--- src/pycheck/exercises/cdd3b0f6ed75e98dd028f95aa7546eda.py
+++ src/pycheck/exercises/cdd3b0f6ed75e98dd028f95aa7546eda.py
@@ -3,6 +3,8 @@ TITLE = 'El cuadrado rojo'
 DESCRIPTION = '''
 Calcule el área del cuadrado rojo dada la longitud del arco _A_. Use _PI_ con 2 cifras decimales.
 
+IMPORTANTE: Redondee el resultado a 10 cifras decimales.
+
 [Imagen del cuadrado rojo](https://i.imgur.com/nJrae8n.png)
 '''
 
@@ -16,9 +18,9 @@ ENTRYPOINT = {
 }
 
 CHECK_CASES = [
-    [[1], [0.40569597143900354]],
-    [[0.25], [0.02535599821493772]],
-    [[9.99], [40.488498519209706]],
+    [[1], [0.4056959714]],
+    [[0.25], [0.0253559982]],
+    [[9.99], [40.4884985192]],
 ]
 
 SOURCE = 'https://www.codewars.com/kata/5748838ce2fab90b86001b1a'
