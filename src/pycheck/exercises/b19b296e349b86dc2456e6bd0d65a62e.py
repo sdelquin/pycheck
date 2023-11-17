b971446c46e0dbed58094b86f9f8061ee33aeba7
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-17 16:53:16 +0000

    Fix description of exercise

--- src/pycheck/exercises/b19b296e349b86dc2456e6bd0d65a62e.py
+++ src/pycheck/exercises/b19b296e349b86dc2456e6bd0d65a62e.py
@@ -2,6 +2,9 @@ TITLE = 'Convirtiendo ADN a ARN'
 
 DESCRIPTION = """
 Escribe un programa en Python que realice la conversión de una secuencia de ADN en su correspondiente ARN. El ARN es igual que el ADN salvo que la Timina se sustituye por Uracilo (`U`).
+
+NOTA:
+- No se puede utilizar la función _replace()_
 """
 
 ENTRYPOINT = {
