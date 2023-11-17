911f90c547ad4925942947e5c1d3af7b9e296081
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-17 17:11:48 +0000

    Fix minor issue on assignment

--- src/pycheck/exercises/dc1245e813ad4d977e74a0da79214de7.py
+++ src/pycheck/exercises/dc1245e813ad4d977e74a0da79214de7.py
@@ -1,7 +1,9 @@
 TITLE = 'Left strip'
 
 DESCRIPTION = """
-Implemente la función **lstrip()** de Python sin utilizar la función _lstrip()_. Es decir, dada una cadena de texto de entrada y otra cadena representando los caracteres a _"stripear"_ (eliminar) se debe devolver una nueva cadena de texto eliminando del principio aquellos caracteres indicados.
+Implemente el mismo comportamiento de la función **lstrip()** de Python sin utilizar ninguna variante de las funciones _strip()_ existentes en Python.
+
+Es decir, dada una cadena de texto de entrada y otra cadena representando los caracteres a _"stripear"_ (eliminar) se debe devolver una nueva cadena de texto eliminando del principio aquellos caracteres indicados.
 """
 
 ENTRYPOINT = {
