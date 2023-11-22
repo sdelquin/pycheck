a29a18f5c667b58d6890e3db1430ab38a1814802
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-11-22 17:02:17 +0000

    Add new check cases

--- src/pycheck/exercises/f552a777bf51e6fe1f7f7039bd232793.py
+++ src/pycheck/exercises/f552a777bf51e6fe1f7f7039bd232793.py
@@ -1,8 +1,8 @@
 TITLE = 'Desplegando caracteres'
 
-DESCRIPTION = '''
+DESCRIPTION = """
 Dada una lista de strings, obtenga otra lista que contenga todos los caracteres de cada uno de los strings de la lista de entrada.
-'''
+"""
 
 ENTRYPOINT = {
     'PARAMS': [
@@ -17,4 +17,6 @@ CHECK_CASES = [
     [[['uno', 'dos', 'tres']], [['u', 'n', 'o', 'd', 'o', 's', 't', 'r', 'e', 's']]],
     [[['', '', '']], [[]]],
     [[['abc', 'abc', 'abc']], [['a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c']]],
+    [[['XZ', 'VW']], [['X', 'Z', 'V', 'W']]],
+    [[['alone']], [['a', 'l', 'o', 'n', 'e']]],
 ]
