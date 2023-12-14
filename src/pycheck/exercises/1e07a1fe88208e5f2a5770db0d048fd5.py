d0fc7d83d6b9097763f85452e2c2904aa474b19b
Sergio Delgado Quintero <sdelquin@gmail.com>
2023-12-14 20:18:07 +0000

    Fix exercise

--- src/pycheck/exercises/1e07a1fe88208e5f2a5770db0d048fd5.py
+++ src/pycheck/exercises/1e07a1fe88208e5f2a5770db0d048fd5.py
@@ -1,10 +1,13 @@
 TITLE = 'Suma de la diagonal principal'
 
-DESCRIPTION = '''
+DESCRIPTION = """
 Escriba un programa en Python que acepte una lista de listas representando una matriz numérica de valores enteros y compute la suma de los elementos de la diagonal principal.
 
-Lo puede afrontar en _versión clásica_ o con _listas por comprensión_. ¡O incluso ambas!
-'''
+Notas:
+- Si no se trata de una [Matriz cuadrada](https://es.wikipedia.org/wiki/Matriz_cuadrada) devuelva None.
+- Puede afrontar el ejercicio en _versión clásica_ o con _listas por comprensión_. ¡O incluso ambas!
+
+"""
 
 ENTRYPOINT = {
     'PARAMS': [
@@ -18,5 +21,6 @@ ENTRYPOINT = {
 CHECK_CASES = [
     [[[[4, 6, 1], [2, 9, 3], [1, 7, 7]]], [20]],
     [[[[1, 1], [1, 1]]], [2]],
-    [[[[1, 2, 3, 4], [4, 5, 6, 7], [8, 9, 10, 11]]], [16]],
+    [[[[1, 2, 3, 4], [4, 5, 6, 7], [8, 9, 10, 11], [4, 2, 1, 8]]], [24]],
+    [[[[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]]], [None]],
 ]
