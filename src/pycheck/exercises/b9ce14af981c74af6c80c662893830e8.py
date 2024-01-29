e0db2622369362d7f5f8656389ff2e72628bb901
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-01-29 15:55:43 +0000

    Fix return value

--- src/pycheck/exercises/b9ce14af981c74af6c80c662893830e8.py
+++ src/pycheck/exercises/b9ce14af981c74af6c80c662893830e8.py
@@ -1,21 +1,21 @@
 TITLE = 'Tuplas y conjuntos'
 
-DESCRIPTION = '''
+DESCRIPTION = """
 Dada una tupla de duplas (2 valores) obtenga una tupla con dos conjuntos:
 - El primer conjunto con los primeros valores de cada dupla.
 - El segundo conjunto con los segundos valores de cada dupla.
-'''
+"""
 
 ENTRYPOINT = {
     'PARAMS': [
         ['input', tuple],
     ],
     'RETURN': [
-        ['output', set],
+        ['output', tuple],
     ],
 }
 
 CHECK_CASES = [
     [[((4, 3), (8, 2), (7, 5), (8, 2), (9, 1))], [({8, 9, 4, 7}, {1, 2, 3, 5})]],
-    [[((1, 2), ('|', '@'), ('!', '\"'))], [({1, '!', '|'}, {2, '"', '@'})]],
+    [[((1, 2), ('|', '@'), ('!', '"'))], [({1, '!', '|'}, {2, '"', '@'})]],
 ]
