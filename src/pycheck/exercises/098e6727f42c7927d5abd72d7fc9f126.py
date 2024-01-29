342b06d56518cff389e04a5f8b40b554c79b437f
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-01-29 18:08:32 +0000

    Add exercises

---
+++ src/pycheck/exercises/098e6727f42c7927d5abd72d7fc9f126.py
@@ -0,0 +1,20 @@
+TITLE = 'Palabra diversa'
+
+DESCRIPTION = """
+Dada una lista de palabras, indique cuál de todas ellas es la más diversa, entendiendo por diversa **la que más letras distintas del abecedario utiliza**.
+"""
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['words', list],
+    ],
+    'RETURN': [
+        ['dword', str],
+    ],
+}
+
+CHECK_CASES = [
+    [[['dictionary', 'turtle', 'flexibility', 'humanity']], ['dictionary']],
+    [[['light', 'environment', 'watermelon', 'happiness']], ['watermelon']],
+    [[['telescope', 'blackboard', 'microprocessor', 'destination']], ['microprocessor']],
+]
---
+++ src/pycheck/exercises/b4fcea707f5d1dc953074e6899ea6718.py
@@ -0,0 +1,23 @@
+TITLE = 'Mitad fuera'
+
+DESCRIPTION = """
+Se recibe un conjunto de números enteros _A_ y se debe generar otro conjunto _B_ tal que:
+
+- Los valores de _B_ son los que están en _A_ división entera por 2.
+- Si el resultado de la división es un valor que existe en _A_ no se debe incluir en _B_.
+"""
+
+ENTRYPOINT = {
+    'PARAMS': [
+        ['values', set],
+    ],
+    'RETURN': [
+        ['half_out_values', set],
+    ],
+}
+
+CHECK_CASES = [
+    [[{6, 12, 4, 50, 100}], [{3, 2, 25}]],
+    [[{42, 21, 14, 18, 7}], [{9, 10, 3}]],
+    [[{11, 8, 37, 21, 84, 5}], [{2, 4, 10, 42, 18}]],
+]
