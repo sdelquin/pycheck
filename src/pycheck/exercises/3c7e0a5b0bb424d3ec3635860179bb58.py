3206694770072498968de591a27fcf04a3c7dc92
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-03-18 09:11:16 +0000

    Fix exercise

--- src/pycheck/exercises/3c7e0a5b0bb424d3ec3635860179bb58.py
+++ src/pycheck/exercises/3c7e0a5b0bb424d3ec3635860179bb58.py
@@ -1,15 +1,15 @@
 TITLE = 'Decorando con valores absolutos'
 
-DESCRIPTION = '''
-Escriba un decorador llamado _fabs()_ que convierta a su **valor absoluto** los dos primeros parámetros de la función que decora y devuelva el resultado de aplicar dicha función a sus dos argumentos. Aplique este decorador a la función _fprod()_ que es el punto de entrada del programa.
+DESCRIPTION = """
+Escriba un decorador llamado _fabs()_ que convierta a su **valor absoluto** los dos primeros parámetros de la función que decora y devuelva el resultado de aplicar dicha función a sus dos argumentos. Aplique este decorador a la función _add()_ que es el punto de entrada del programa (y que suma dos valores).
 
 _¿Podría extender el decorador para que tuviera en cuenta un número indeterminado de argumentos posicionales?_
-'''
+"""
 
 EMPTY_TEMPLATE = True
 
 ENTRYPOINT = {
-    'NAME': 'fprod',
+    'NAME': 'add',
     'PARAMS': [
         ['a', int],
         ['b', int],
@@ -20,8 +20,8 @@ ENTRYPOINT = {
 }
 
 CHECK_CASES = [
-    [[-3, 7], [21]],
-    [[-3, -9], [27]],
-    [[-1, 1], [1]],
-    [[0, -10], [0]],
+    [[-3, 7], [10]],
+    [[-3, -9], [12]],
+    [[-1, 1], [2]],
+    [[4, -10], [14]],
 ]
